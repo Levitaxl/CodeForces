2185560eba55040bc0e202d323d4099f0de159b6
hssanchez17 <hssanchez.17@est.ucab.edu.ve>
2020-04-05 12:47:14 -0430

    1220A

--- P1220A.java
+++ P1220A.java
@@ -1,26 +1,36 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class P1220A {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int n;
-        String n2;
+        int n0=0;
+        int n1=0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Ya tenemos el "lector"
+        StringBuilder n2= new StringBuilder();
+        StringBuilder n3= new StringBuilder();
+    
         
+        n3.append(br.readLine());
+        n=Integer.parseInt(n3.toString());
         
-        Scanner s = new Scanner(System.in);
-        n = s.nextInt();
-        n2= s.nextLine();
+        n2.append(br.readLine());
         
-        for (int i = 1; i <=n; i++) {
-            if(n2.charAt(i)== 'z')System.out.print("0 ");
-            if(n2.charAt(i)== 'n')System.out.print("1 ");
+        for (int i = 0; i <n; i++) {
+            if(n2.charAt(i)== 'z')n0++;
+            else if(n2.charAt(i)== 'n') n1++;
         }   
         
-        //for (int i = 0; i < n1; i++) System.out.print("1 ");
-        //for (int i = 0; i < n0; i++) System.out.print("0 ");
-            
-        
-    
+        while(n1>0){
+            n1--;
+            System.out.print("1 ");}
+        while(n0>0){
+            n0--;
+            System.out.print("0 ");}
   }
     
 }
