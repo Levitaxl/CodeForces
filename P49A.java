c138dac01d918877bb0b89db0d066bd046b3651c
hssanchez17 <hssanchez.17@est.ucab.edu.ve>
2020-04-26 19:10:51 -0430

    P49A

---
+++ P49A.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc
+ */
+public class P49A {
+    public static void main (String[] args) { 
+       Scanner entrada = new Scanner(System.in);    
+       String s = (entrada.nextLine()).toLowerCase();
+       int i=s.length()-1;
+       
+       while(i>=0){
+           if(s.charAt(i)!=' ' && s.charAt(i)!='?'){
+               if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='y')    System.out.println("YES");
+               else System.out.println("NO");
+               break;
+           }
+           i--;
+       
+       
+    }
+}
+}
