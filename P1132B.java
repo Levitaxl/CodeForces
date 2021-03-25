45f673c90706143ca1007a8fb3ef3a24ccdd3d55
Levitaxl <hermessamuel10@gmail.com>
2021-03-25 13:10:25 -0600

    P1132B.java

--- P1132B.java
+++ P1132B.java
@@ -1,67 +1,157 @@
-
-import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-    
 public class P1132B {
-    
-   /* public static void main (String[] args) { 
-        Scanner entrada = new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Ya tenemos el "lector"
-        int n=entrada.nextInt();
-        int []a= new int[n];
-        long sum=0;
-        
-        for (int i = 0; i < n; i++){ 
-            a[i]=entrada.nextInt();
-            sum=a[i]+sum;
+     static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+ 
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
         }
-        
-        Arrays.sort(a);        
-        
-        int m=entrada.nextInt();
-        //int []q= new int[m];
-        
-        for (int i = 0; i < m; i++) {
-            int q=entrada.nextInt();
-            long sum2=sum-a[n-q];
-            System.out.println(sum2);
+ 
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(
+                new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
         }
-       
-        
-        
-        
-        
-    
-    }*/
+ 
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+ 
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+ 
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                                 BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+ 
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+ 
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+ 
     
         public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-            StringTokenizer st = new StringTokenizer(br.readLine()); 
-            int n = Integer.parseInt(st.nextToken());
-            int[]a= new int[n];
+            Reader entrada = new Reader();
+            int n=entrada.nextInt();
+            int[] a= new int[n];
             long sum=0;
-            System.out.println(n);
             
-            for (int i = 0; i < n; i++){ 
-                a[i]=Integer.parseInt(st.nextToken()); 
-                
-                //sum=a[i]+sum;
+            for (int i = 0; i < n; i++){
+                a[i]=entrada.nextInt();
+                sum+=a[i];
             }
-            Arrays.sort(a); 
-           System.out.println(Arrays.toString(a));
+
+            int m=entrada.nextInt();
+            int[] q= new int[m];
+            for (int i = 0; i < m; i++)q[i]=entrada.nextInt();
+          
+            Arrays.sort(a);
             
-            /*int m=Integer.parseInt(st.nextToken());
+            for (int i = 0; i < m; i++){
+                int complemento=n-q[i];
+                System.out.println(sum-a[complemento]);
+            }
+
             
-            
-            for (int i = 0; i < m; i++) {
-                int q=Integer.parseInt(st.nextToken());
-                long sum2=sum-a[n-q];
-                System.out.println(sum2);
-            }*/
+                
+             
             
             
         }
