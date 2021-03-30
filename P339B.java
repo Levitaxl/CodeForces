2ab413bec2ba49ba5eb4a2e58567bfbb4799ac1a
Levitaxl <hermessamuel10@gmail.com>
2021-03-30 11:55:38 -0600

    P339B .,)

--- P339B.java
+++ P339B.java
@@ -5,26 +5,15 @@ import java.util.Scanner;
 public class P339B {
      public static void main (String[] args) { 
         Scanner entrada = new Scanner(System.in);
-        long n= entrada.nextLong(), m=entrada.nextLong();
-        long []a=new long[(int)m];
-        
-         for (int i = 0; i < m; i++) a[i]=entrada.nextLong();
-         
-         
-         int sum=0;
-         int aux=1;
-         for (int i = 0; i < m; i++) {
-             //if(aux>a[i]) sum=sum+n-aux+a[i];
-             //else sum=sum+a[i]-aux;
-             
-           //  aux=a[i];
-             
-         }
-         
-         System.out.println(sum);
-        
-
+        int n= entrada.nextInt(), m=entrada.nextInt();
+        long sum=0;
+        int prevHouse=1;
+        for (int i = 0; i < m; i++) {
+            int nextHouse=entrada.nextInt();
+            if(prevHouse<nextHouse)        sum+=nextHouse-prevHouse;
+            else if(prevHouse>nextHouse)   sum+=n-(prevHouse-nextHouse);           
+            prevHouse=nextHouse;  
+        }
+        System.out.println(sum);
     }
-    
-    
 }
